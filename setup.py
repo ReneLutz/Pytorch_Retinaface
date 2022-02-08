8468f090e3aebe1eaeb017949ad5ad1cdc2b2bda
Rene Lutz <rene.lutz@sit.fraunhofer.de>
2022-02-08 14:04:19 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+requirements = [
+    'torch',
+    'numpy',
+    'opencv-python',
+]
+
+setup(
+    name='pytorch_retinaface',
+    version='1.0',
+
+    description="Python package for Pytorch_Retinaface implementation by biubug6 (https://github.com/biubug6/Pytorch_Retinaface)",
+
+    url='https://github.com/ReneLutz/Pytorch_Retinaface',
+    author='Rene Lutz',
+
+    packages=find_packages(),
+
+    classifiers=[
+        'Intended Audience :: Developers',
+
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.8',
+    ]
+)
